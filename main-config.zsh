90731937f5299d39275776ddc5c364b4aa8a5a12
leo-chen <S8O9R6@u.northwestern.edu>
2023-09-14 10:23:38 -0500

    Start the repo with basic set of NW setup.zsh and a self-install ansible playbook

---
+++ main-config.zsh
@@ -0,0 +1,12 @@
+#! /usr/bin/zsh
+
+# Source the ros stuff
+
+# Yes using ros2 iron version
+source_if_exists "/opt/ros/iron/setup.zsh" true
+
+# TODO split this kind of things later
+
+source_if_exists "${HOME}/ws/interbotix/install/setup.zsh" true
+
+
