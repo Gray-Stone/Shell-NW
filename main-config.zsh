bc5369b5f4cd6c290678f4ea47b6a7234daac7b0
leo-chen <S8O9R6@u.northwestern.edu>
2023-09-28 10:51:13 -0500

    Add some ntoes about auto complete

--- main-config.zsh
+++ main-config.zsh
@@ -1,12 +1,29 @@
 #! /usr/bin/zsh
 
-# Source the ros stuff
+# Source the ros stuff and include a waring print out 
 
 # Yes using ros2 iron version
 source_if_exists "/opt/ros/iron/setup.zsh" true
 
-# TODO split this kind of things later
+# This is from previous interbotix stuff
+# source_if_exists "${HOME}/ws/interbotix/install/setup.zsh" true
 
-source_if_exists "${HOME}/ws/interbotix/install/setup.zsh" true
+CURRENT_ROS_LOCAL_WORKSPACE="$(realpath ${HOME}/495-hw-workspace)"
 
+source_if_exists "${CURRENT_ROS_LOCAL_WORKSPACE}/install/setup.zsh" true
+
+function nw-cbuild()
+{
+    (set -vx ; cd "${CURRENT_ROS_LOCAL_WORKSPACE}" && colcon build --symlink-install $@ )
+}
+
+
+# argcomplete for ros2 & colcon
+# This link does the trick. I think it's the autoload doing the work 
+# https://github.com/ros2/ros2cli/issues/534#issuecomment-957926737
+
+
+# echo "Regiested Ros2 colcon argcomplete"
+# eval "$(register-python-argcomplete3 ros2)"
+# eval "$(register-python-argcomplete3 colcon)"
 
